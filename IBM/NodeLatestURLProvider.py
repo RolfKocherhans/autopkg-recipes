73c0a6ec42213456cab36623b791a498e65b98e0
Rolf Kocherhans <rolf.kocherhans@gmail.com>
2016-08-26 09:52:37 +0200

    Update NodeLatestURLProvider.py

--- IBM/NodeLatestURLProvider.py
+++ IBM/NodeLatestURLProvider.py
@@ -42,7 +42,7 @@ class NodeLatestURLProvider(Processor):
     
     def main(self):
         url = """osascript -e '
-        -- extract the name of the latest major "Tivoli Storage Manager" version e.g. v6r3,v6r4,v7r1 -> v7r1
+        -- # extract the name of the latest major "Tivoli Storage Manager" version e.g. v6r3,v6r4,v7r1 -> v7r1
         set ftpServer to "ftp://public.dhe.ibm.com"
         set ftpDirectory to "/storage/tivoli-storage-management/maintenance/client/"
         set shellCommand to "curl " & quoted form of (ftpServer & ftpDirectory)
@@ -55,18 +55,22 @@ class NodeLatestURLProvider(Processor):
         set folderNames to paragraphs of (do shell script shellCommand)
         set varMinorVersion to last word of last item of folderNames --as string
 
+        -- # add this info to the URL and go one levels deeper and extrct the file name e.g. 7.1.6.0-TIV-TSMBAC-Mac.dmg
         set ftpDirectory to "/storage/tivoli-storage-management/maintenance/client/" & varMajorVersion & "/Mac/" & varMinorVersion & "/"
         set shellCommand to "curl " & quoted form of (ftpServer & ftpDirectory)
         set folderNames to paragraphs of (do shell script shellCommand)
-
+        
+        -- # extract line containing file name info "-rw-r--r--    1 102004493 209       156000727 Jun 17 10:41 7.1.6.0-TIV-TSMBAC-Mac.dmg"
         set AppleScript's text item delimiters to {space}
         set delimitedList to every text item of folderNames
         set fileNameWholeLine to text item 3 of delimitedList
 
+        -- # extract the file name from the line conating it e.g. 7.1.6.0-TIV-TSMBAC-Mac.dmg
         set AppleScript's text item delimiters to {space}
         set delimitedList to every text item of fileNameWholeLine
         set fileName to last text item of delimitedList
 
+        -- # create download link
         set downloadLink to ftpServer & "/storage/tivoli-storage-management/maintenance/client/" & varMajorVersion & "/Mac/" & varMinorVersion & "/" & fileName"'"""
         self.env["url"] = url      
    
