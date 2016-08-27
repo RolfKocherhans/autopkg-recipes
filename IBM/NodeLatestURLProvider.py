7a99b13e76905c5a5886a13c392dd5a3bcb732d6
Rolf Kocherhans <rolf.kocherhans@gmail.com>
2016-08-27 23:42:48 +0200

    Update NodeLatestURLProvider.py

--- IBM/NodeLatestURLProvider.py
+++ IBM/NodeLatestURLProvider.py
@@ -24,16 +24,6 @@ __all__ = ["NodeLatestURLProvider"]
 
 
 class NodeLatestURLProvider(Processor):
-    description = "Returns url to the latest Tivoli Storage Manager package."
-    input_variables = {
-        "url": {
-            "description": "download URL.",
-        }
-    }
-    
-    __doc__ = description
-    
-
     def main(self):
         url = subprocess.check_output(['osascript', '-e', r'''
         -- extract the name of the latest major "Tivoli Storage Manager" version e.g. v6r3,v6r4,v7r1 -> v7r1
